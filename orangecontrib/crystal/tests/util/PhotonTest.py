7200038f00e268f88b9284b4ee46b92c49577242
Mark Glass <glass@lin-cb101.esrf.fr>
2014-11-14 17:46:17 +0100

    Remove quantities module from Photon class.

--- orangecontrib/crystal/tests/util/PhotonTest.py
+++ orangecontrib/crystal/tests/util/PhotonTest.py
@@ -1,54 +1,58 @@
+"""
+Unittest for Photon class.
+"""
+
 import unittest
-import numpy as np
-from quantities import *
 
 from orangecontrib.crystal.util.Photon import Photon
 from orangecontrib.crystal.util.Vector import Vector
 
 class PhotonTest(unittest.TestCase):
     def testConstructor(self):
-        photon = Photon(4 * keV, Vector(0, 0, 1))
+        photon = Photon(4000, Vector(0, 0, 1))
 
-        self.assertEqual(photon.energy(), 4 * keV)
+        self.assertEqual(photon.energy(), 4000)
         self.assertTrue(photon.unitDirectionVector() == Vector(0, 0, 1))
 
     def testEnergy(self):
-        photon = Photon(4 * keV, Vector(0, 0, 1))
-        self.assertEqual(photon.energy(), 4 * keV)
+        photon = Photon(4000, Vector(0, 0, 1))
+        self.assertEqual(photon.energy(), 4000)
 
     def testWavelength(self):
-        test_data = {3 * eV : 413.28 * nm,
-                     4 * eV : 309.96 * nm,
-                     8 * eV : 154.98 * nm,
-                     5 * keV: 2.4797 * angstrom,
-                    10 * keV: 1.2398 * angstrom}
+        # Test data in eV : m.
+        test_data = {   3 : 413.28 * 10e-9,
+                        4 : 309.96 * 10e-9,
+                        8 : 154.98 * 10e-9,
+                     5000 : 2.4797 * 10e-10,
+                    10000 : 1.2398 * 10e-10}
 
-        for energy, wavelength in test_data.iteritems():
+        for energy, wavelength in test_data.items():
             photon = Photon(energy, Vector(0, 0, 1))
             self.assertAlmostEqual(photon.wavelength(),
                                    wavelength, 2)
 
     def testWavenumber(self):
-        test_data = {3 * eV : 152031.93111 * cm ** -1,
-                     4 * eV : 202709.24149 * cm ** -1,
-                     8 * eV : 405418.48298 * cm ** -1,
-                     5 * keV: 253386541.53 * cm ** -1,
-                    10 * keV: 506773083.07 * cm ** -1}
+        # Test data in eV : m^-1.
+        test_data = {   3 : 15203192.8,
+                        4 : 20270923.76,
+                        8 : 40541847.5,
+                     5000 : 25338654707.5,
+                    10000 : 50677309415}
 
 
-        for energy, wavenumber in test_data.iteritems():
+        for energy, wavenumber in test_data.items():
             photon = Photon(energy, Vector(0, 0, 1))
             self.assertAlmostEqual(photon.wavenumber(),
                                    wavenumber, 1)
 
     def testWavevector(self):
         direction = Vector(0, 0, 1)
-        photon = Photon(5 * keV, direction)
+        photon = Photon(5000.0, direction)
 
         wavevector = photon.wavevector()
 
-        self.assertAlmostEqual(wavevector.norm().rescale(um **-1),
-                               25338.655186 * um ** -1, 1)
+        self.assertAlmostEqual(wavevector.norm(),
+                               25338654707.5, 1)
 
         self.assertEqual(wavevector.getNormalizedVector(),
                          direction)
--- orangecontrib/crystal/util/Photon.py
+++ orangecontrib/crystal/util/Photon.py
@@ -1,37 +1,76 @@
-import numpy as np
-from quantities import *
 import scipy.constants.codata
+
+"""
+Represents a photon.
+Except for energy all units are in SI. Energy is in eV.
+"""
 
 class Photon():
 
-    def __init__(self, energy, direction_vector):
-        self._energy = energy
+    def __init__(self, energy_in_ev, direction_vector):
+        """
+        Constructor.
+        :param energy_in_ev: Photon energy in eV.
+        :param direction_vector: The direction of the photon as Vector.
+        """
+        self._energy_in_ev = float(energy_in_ev)
         self._unit_direction_vector = direction_vector.getNormalizedVector()
 
     def energy(self):
-        return self._energy
+        """
+        :return: Energy in eV.
+        """
+        return self._energy_in_ev
 
     def wavelength(self):
+        """
+        :return: The photon wavelength in meter.
+        """
         codata = scipy.constants.codata.physical_constants
-        codata_c, tmp1, tmp2 = codata["speed of light in vacuum"]
-        codata_h, tmp1, tmp2 = codata["Planck constant"]
-        E_in_Joule = float(self.energy().rescale(J).magnitude)
+        speed_of_light = codata["speed of light in vacuum"][0]
+        planck_constant = codata["Planck constant"][0]
+        elementary_charge=codata["elementary charge"][0]
+        E_in_Joule = self.energy() * elementary_charge
 
-        wavelength = (codata_c * codata_h / E_in_Joule) * meter
+        # Wavelength in meter
+        wavelength = (speed_of_light * planck_constant / E_in_Joule)
 
         return wavelength
 
     def wavenumber(self):
-        return (2.0 * np.pi) / self.wavelength()
+        """
+        :return: Wavenumber in m^-1.
+        """
+        return (2.0 * scipy.constants.codata.pi) / self.wavelength()
 
     def wavevector(self):
+        """
+        :return: Photon wavevector in m^-1.
+        """
         return self.unitDirectionVector().scalarMultiplication(self.wavenumber())
 
     def unitDirectionVector(self):
+        """
+        :return: Photon direction.
+        """
         return self._unit_direction_vector
 
     def __eq__(self, candidate):
-        return self.wavenumber() == candidate.wavenumber() and self.unitDirectionVector() == candidate.unitDirectionVector()
+        """
+        Determines if two photons are identical (same energy and direction).
+        :param candidate: Photon to compare with.
+        :return: True if equal otherwise False.
+        """
+        is_equal = self.energy() == candidate.energy() \
+                   and \
+                   self.unitDirectionVector() == candidate.unitDirectionVector()
+
+        return is_equal
 
     def __ne__(self, candidate):
+        """
+        Determines if two photons are not identical (same energy and direction).
+        :param candidate: Photon to compare with.
+        :return: True if not equal otherwise False.
+        """
         return not (self==candidate)
